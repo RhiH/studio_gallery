cc4f5645684b5885b1ec73a123b371574b10b90e
RhiH <rhi.herbert@gmail.com>
2023-03-05 19:23:12 +0000

    template for artists bios added

--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from products.models import Category
 
 # Create your models here.
+
+class Creator(models.Model):
+    name = models.ForeignKey(Category, null=False, blank=False, on_delete=models.CASCADE)
+
+class Biography(models.Model):
+    bio = models.CharField(max_length=200)
--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    template = 'artists/artists.html'
+
+    return render(request, template)
