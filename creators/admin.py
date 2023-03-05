7424544d3cf39fa9d0ff9e26e313c5ff59d817e0
RhiH <rhi.herbert@gmail.com>
2023-03-05 15:53:47 +0000

    remove creators app

--- creators/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ArtistsInfo, Category
-
-# Register your models here.
-
-admin.site.register(ArtistsInfo)
-admin.site.register(Category)
--- creators/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_artists, name='artists'),
-    # path('<artist_id>', views.artists_detail, name='artist_detail'),
-]
