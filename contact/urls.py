39d943e00301a7546cb02c01239d9e34a718f3be
RhiH <rhi.herbert@gmail.com>
2023-03-06 21:55:48 +0000

    create amendments functionality for super users

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('', views.contact, name='contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
+from .forms import ContactForm
 
-def index(request):
+def contact(request):
+    """ contact page """
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for your message')
+        else:
+            messages.error(request, 'Failed to send message. Please ensure the form is valid.')
+    form = ContactForm()
+
     template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
 
-    return render(request, template)
+    return render(request, template, context)
