64905944fa4173edcac5f815d0eeb880b56c598f
RhiH <rhi.herbert@gmail.com>
2023-03-07 20:16:00 +0000

    add, edit and delete products functionality added

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .forms import ContactForm
@@ -6,17 +6,18 @@ from .forms import ContactForm
 def contact(request):
     """ contact page """
     if request.method == 'POST':
-        form = ContactForm(request.POST, request.FILES)
+        form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Thank you for your message')
         else:
             messages.error(request, 'Failed to send message. Please ensure the form is valid.')
-    form = ContactForm()
+    else:
+        form = ContactForm()
 
     template = 'contact/contact.html'
     context = {
         'form': form,
     }
-
+    
     return render(request, template, context)
