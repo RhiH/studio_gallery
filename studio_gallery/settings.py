40d97372c5dc2eb7d03edf16a7c4a5e37ff43279
RhiH <rhi.herbert@gmail.com>
2023-03-08 21:07:13 +0000

    admendments to settings file

--- studio_gallery/settings.py
+++ studio_gallery/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+(&5fdaut64u53+-kwf83v#t#f86+m+fqoy^dpu5kvd35-!&#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studio-gallery.herokuapp.com', 'localhost']
 
 
 # Application definition
