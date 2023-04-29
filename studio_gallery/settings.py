0412b5a0e1ef6e75f8f1ea408b17bafbce073a96
RhiH <rhi.herbert@gmail.com>
2023-04-29 19:29:03 +0000

    test debug for email failure

--- studio_gallery/settings.py
+++ studio_gallery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['studio-gallery.herokuapp.com', 'localhost']
 
@@ -227,7 +227,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'rhih31@googlemail.com'
+    DEFAULT_FROM_EMAIL = 'theartists@studiogallery.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
