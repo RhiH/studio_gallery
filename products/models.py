2ac31f7c76278f7843a615386cc71af47a296533
RhiH <rhi.herbert@gmail.com>
2023-05-09 19:19:18 +0000

    update to product descriptions in json file

--- products/models.py
+++ products/models.py
@@ -23,9 +23,10 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
+    
 
     def __str__(self):
         return self.name
