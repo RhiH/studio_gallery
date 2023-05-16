a0e77c2ac7c27eb6ce268359213b5103d2e99ce0
RhiH <rhi.herbert@gmail.com>
2023-05-16 18:12:59 +0000

    test link to artist category amendment

--- studio_gallery/settings.py
+++ studio_gallery/settings.py
@@ -125,12 +125,7 @@ WSGI_APPLICATION = 'studio_gallery.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
