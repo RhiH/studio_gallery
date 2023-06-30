6c3c3063255e2f4a6470bd46806dad3016215434
RhiH <rhi.herbert@gmail.com>
2023-06-30 18:45:19 +0000

    migrations error - unfound coloumn fix and remove foregin key categories in artists tes

--- artists/models.py
+++ artists/models.py
@@ -4,14 +4,14 @@ from products.models import Product
 
 # Create your models here.
 
-class Artist(models.Model):
+class Artists(models.Model):
 
     class Meta:
         verbose_name_plural = 'Artists'
 
-    category = models.ForeignKey(
-        Product, null=True, blank=True, on_delete=models.SET_NULL
-        )
+    #category = models.ForeignKey(
+    #    Product, null=True, blank=True, on_delete=models.SET_NULL
+    #    )
     name = models.CharField(max_length=254)
     country = models.CharField(max_length=500, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
