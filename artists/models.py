24c3869ae0e560dd82339d50454144a5f26b00fd
RhiH <rhi.herbert@gmail.com>
2023-07-01 20:38:15 +0000

    amended artist model to remove category and load artist ifnfo fixture

--- artists/models.py
+++ artists/models.py
@@ -1,17 +1,10 @@
 from django.db import models
-from products.models import Product
-
 
 # Create your models here.
 
-class Artist(models.Model):
 
-    class Meta:
-        verbose_name_plural = 'Artists'
+class Artists(models.Model):
 
-    category = models.ForeignKey( 
-       Product, null=True, blank=True, on_delete=models.SET_NULL
-       )
     name = models.CharField(max_length=254)
     country = models.CharField(max_length=500, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
